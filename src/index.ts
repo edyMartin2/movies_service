847ff405e7c768308e8915a15dc43ca2edb08325
edyMartin <edgar.sdc@hotmail.com>
2023-08-17 21:10:58 -0600

    enrutado6

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,13 @@ import Movies from './models/MoviesModel'
 import { ObjectId } from 'mongodb'
 import Plataforms from './models/PlataformModel'
 import cors from 'cors'
+import Repository from './infrastructure/Repository'
 
 const app = express()
 const PORT = 3000
 const moviesRp = new MoviesApplication()
 const plataformRP = new PlataformApplication()
+const repository = Repository.getInstance()
 // Middleware
 app.use(bodyParser.json());
 app.use(cors())
@@ -22,80 +24,80 @@ app.use(cors())
 /**
  * endpoint / for get all movies
  */
-app.get('/api/movies', async (_, res) => {
-  const movies: Movies = await moviesRp.findAll()
-  res.json(movies)
-});
+// app.get('/api/movies', async (_, res) => {
+//   const movies: Movies = await moviesRp.findAll()
+//   res.json(movies)
+// });
 
-/**
- * endpoint /movies/:id for get one movie
- */
-app.get('/api/movies/:id', async (req, res) => {
-  const id = req.params.id
-  const movie: Movies = await moviesRp.findById(id)
-  res.json(movie)
-});
+// /**
+//  * endpoint /movies/:id for get one movie
+//  */
+// app.get('/api/movies/:id', async (req, res) => {
+//   const id = req.params.id
+//   const movie: Movies = await moviesRp.findById(id)
+//   res.json(movie)
+// });
 
-/**
- * endpoint /movies for create one movie
- */
-app.post('/api/movies', async (req, res) => {
-  const body: Movies = req.body
-  const save_movie = await moviesRp.create(body)
-  res.json(save_movie)
-})
+// /**
+//  * endpoint /movies for create one movie
+//  */
+// app.post('/api/movies', async (req, res) => {
+//   const body: Movies = req.body
+//   const save_movie = await moviesRp.create(body)
+//   res.json(save_movie)
+// })
 
-/**
- * endpoint /movies/:id for update one movie
- */
-app.post('/api/movies/:id', async (req, res) => {
-  const id: ObjectId = new ObjectId(req.params.id)
-  const body: Movies = req.body
-  const update_movie = await moviesRp.update(id, body)
-  res.json(update_movie)
-})
+// /**
+//  * endpoint /movies/:id for update one movie
+//  */
+// app.post('/api/movies/:id', async (req, res) => {
+//   const id: ObjectId = new ObjectId(req.params.id)
+//   const body: Movies = req.body
+//   const update_movie = await moviesRp.update(id, body)
+//   res.json(update_movie)
+// })
 
-/**
- * endpoint /movies/:id for delete one movie
- */
-app.delete('/api/movies/:id', async (req, res) => {
-  const id: ObjectId = new ObjectId(req.params.id)
-  console.log(id)
-  const delete_movie = await moviesRp.delete(id)
-  res.json(delete_movie)
-})
+// /**
+//  * endpoint /movies/:id for delete one movie
+//  */
+// app.delete('/api/movies/:id', async (req, res) => {
+//   const id: ObjectId = new ObjectId(req.params.id)
+//   console.log(id)
+//   const delete_movie = await moviesRp.delete(id)
+//   res.json(delete_movie)
+// })
 
-//  ******************* Plataforms ************************
+// //  ******************* Plataforms ************************
 
-/**
- * endpoint /platforms for get all plataforms
- */
-app.get('/api/plataforms', async (req, res) => {
-  const plataform: Plataforms = await plataformRP.findAll()
-  console.log('respuesta de pla', plataform)
-  res.json(plataform)
-})
+// /**
+//  * endpoint /platforms for get all plataforms
+//  */
+// app.get('/api/plataforms', async (req, res) => {
+//   const plataform: Plataforms = await plataformRP.findAll()
+//   console.log('respuesta de pla', plataform)
+//   res.json(plataform)
+// })
 
-/**
- * endpoint /plataforms/:id for get one plataform
- */
-app.get('/api/plataforms/:id', async (req, res) => {
-  const id = req.params.id
-  const plataform: Plataforms = await plataformRP.findById(id)
-  res.json(plataform)
-});
+// /**
+//  * endpoint /plataforms/:id for get one plataform
+//  */
+// app.get('/api/plataforms/:id', async (req, res) => {
+//   const id = req.params.id
+//   const plataform: Plataforms = await plataformRP.findById(id)
+//   res.json(plataform)
+// });
 
-/**
- * endpoint /movies for create one movie
- */
-app.post('/api/plataforms', async (req, res) => {
-  const body: Plataforms = req.body
-  const save_movie = await plataformRP.create(body)
-  res.json(save_movie)
-})
+// /**
+//  * endpoint /movies for create one movie
+//  */
+// app.post('/api/plataforms', async (req, res) => {
+//   const body: Plataforms = req.body
+//   const save_movie = await plataformRP.create(body)
+//   res.json(save_movie)
+// })
 
-app.get('/api', async (req, res) => {
-
+app.get('/api/hello', async (req, res) => {
+  console.log('repo')
   res.json({ 'message': "hola mundos" })
 })
 
